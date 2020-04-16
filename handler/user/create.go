26d270f8c5ac87c420e2b06f593f5792e8ce11af
apple <1729282897@qq.com>
2020-04-16 15:07:39 +0800

    错误返回测试1

--- handler/user/create.go
+++ handler/user/create.go
@@ -1,10 +1,9 @@
 package user
 
 import (
-	"fmt"
-	"net/http"
-
+	. "apiserver/handler"
 	"apiserver/pkg/errno"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lexkong/log"
@@ -12,35 +11,37 @@ import (
 
 // Create creates a new user account.
 func Create(c *gin.Context) {
-	var r struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
-	}
-
-	type CreateResponse struct {
-		Username string `json:"username"`
-	}
-
-	var err error
+	var r CreateRequest
 	if err := c.Bind(&r); err != nil {
-		c.JSON(http.StatusOK, gin.H{"error": errno.ErrBind})
+		//c.JSON(http.StatusOK, gin.H{"error": errno.ErrBind})
+		SendResponse(c, errno.ErrBind, nil)
 		return
 	}
 
 	log.Debugf("username is: [%s], password is [%s]", r.Username, r.Password)
 	if r.Username == "" {
-		err = errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not found in db: xx.xx.xx.xx")).Add("This is add message.")
-		log.Errorf(err, "Get an error")
+		//err = errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not found in db: xx.xx.xx.xx")).Add("This is add message.")
+		SendResponse(c, errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not found in db: xx.xx.xx.xx")), nil)
+		//log.Errorf(err, "Get an error")
+		return
 	}
-
-	if errno.IsErrUserNotFound(err) {
-		log.Debug("err type is ErrUserNotFound")
-	}
+	//
+	//if errno.IsErrUserNotFound(err) {
+	//	log.Debug("err type is ErrUserNotFound")
+	//}
 
 	if r.Password == "" {
-		err = fmt.Errorf("password is empty")
+		SendResponse(c, fmt.Errorf("password is empty"), nil)
+		//err = fmt.Errorf("password is empty")
+		return
 	}
 
-	code, message := errno.DecodeErr(err)
-	c.JSON(http.StatusOK, gin.H{"code": code, "message": message})
+	//code, message := errno.DecodeErr(err)
+	//c.JSON(http.StatusOK, gin.H{"code": code, "message": message})
+	rsp := CreateResponse{
+		Username: r.Username,
+	}
+
+	// Show the user information.
+	SendResponse(c, nil, rsp)
 }
